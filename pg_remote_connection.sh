247c32b7686ca7afc5c0957ec50ad0f1bd44cc5d
kharadi-saqib <kharadisaqib@gmail.com>
2023-12-09 12:55:02 +0530

    Script will use to add remote postgresql connection

---
+++ pg_remote_connection.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Specify the PostgreSQL data directory
+PG_DATA_DIR="/etc/postgresql/14/main"
+
+# Specify the IP address, user, database, and authentication method
+IP_ADDRESS="172.31.7.95/32"
+DATABASE="all"
+USER="coderize"
+AUTH_METHOD="md5"
+
+# Step 1: Edit pg_hba.conf
+echo "Editing pg_hba.conf..."
+echo "host    $DATABASE    $USER    $IP_ADDRESS    $AUTH_METHOD" | sudo tee -a "$PG_DATA_DIR/pg_hba.conf"
+
+# Step 2: Reload PostgreSQL
+echo "Reloading PostgreSQL..."
+sudo systemctl reload postgresql
+
+echo "Configuration updated successfully."
